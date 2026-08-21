005f7590ed92e534e0774ed013b61c964b018da7
Kaustubh-Mohapatra <kaustubh.mohapatra@gmail.com>
2026-08-21 15:41:24 +0530

    Integrating motion of servo with readings of IMU

--- firmware/Servo_with_MPU/Servo_with_MPU.ino
+++ firmware/Servo_with_MPU/Servo_with_MPU.ino
@@ -4,12 +4,14 @@
 #include <Adafruit_Sensor.h>
 
 Adafruit_MPU6050 mpu;
-Servo servo;
+Servo rservo;
+Servo lservo;
 
 void setup() {
   Serial.begin(115200);
   Wire.begin(21,22);
-  // servo.attach(25);
+  rservo.attach(25);
+  lservo.attach(33);
 
   if (!mpu.begin(0x68, &Wire)) {
         Serial.println("MPU initialization check failed");
@@ -20,15 +22,14 @@ void setup() {
     else {
         Serial.println("MPU initialization successful");
     }
- //
+
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
 }
 
 void loop() {
-
-  sensors_event_t a = {}; // gets me dem values
+sensors_event_t a = {}; // gets me dem values
   sensors_event_t g = {};
   sensors_event_t temp = {};
   mpu.getEvent(&a, &g, &temp);
@@ -39,8 +40,16 @@ void loop() {
   Serial.print(roll);
   Serial.print(" Pitch: ");
   Serial.print(pitch);
-  Serial.print(" Yaw: ");
-  Serial.print(g.gyro.z);
-  Serial.print(" Temp: ");
-  Serial.println(temp.temperature);
+  Serial.print(" Z axis rotation speed: ");
+  Serial.println(g.gyro.z);
+
+  int servoAngleP = 90 - pitch;
+  servoAngleP = constrain(servoAngleP, 20, 160);
+
+  int servoAngleR = 90 - roll;
+  servoAngleR = constrain(servoAngleR, 20, 160);
+
+  lservo.write(servoAngleP);
+  rservo.write(servoAngleR);
+
 }
